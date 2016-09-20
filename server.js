fd6094d877c5c402751af3af366237477988c7c5
alphinanitta <alphinanitta@gmail.com>
2016-09-20 20:13:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,10 +5,25 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleone = { 
-    title: 'Article One',
-    heading: 'Article One',
-    content : ' This is the first article '
+
+var articles={
+        'article-one' : { 
+            title: 'Article One',
+            heading: 'Article One',
+            content : ' This is the first article '
+        },
+        
+        'article-two ': { 
+            title: 'Article Two',
+            heading: 'Article Two',
+            content : ' This is the second article '
+        },
+        
+        'article-three' :{ 
+            title: 'Article Three',
+            heading: 'Article Three',
+            content : ' This is the third article '
+        }
 };
 
 function createtemplate(data){
@@ -41,17 +56,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-   res.send(createtemplate(articleone));
+app.get('/:ArticleName', function (req, res) {
+   var ArticleName = req.params.ArticleName ;
+   res.send(createtemplate(articles[ArticleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
