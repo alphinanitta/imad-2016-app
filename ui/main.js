03e1d1e0e0f7975ba753f4c8f08d7f9b095a1d58
alphinanitta <alphinanitta@gmail.com>
2016-09-24 07:44:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,11 @@
 console.log('Loaded!');
 
 var element = document.getElementById('main');
-element.innerHTML = 'Hi , i am one and only ADP' ;
+
+element.innerHTML = 'New Value' ;
 
 var img = document.getElementById('madi');
+
 img.onclick = function(){
   img.style.marginLeft = '120px';  
 };
