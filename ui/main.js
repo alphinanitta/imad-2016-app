c616016468cf49d796b4e6299e12008e41744c47
alphinanitta <alphinanitta@gmail.com>
2016-09-27 08:13:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 
 var counter = 0 ; 
-document.getElementById('counter');.onclick = function(){
+document.getElementById('counter').onclick = function(){
   counter= counter + 1 ; 
   var span = document.getElementById('count');
   span.innerHTML = counter.toString();
