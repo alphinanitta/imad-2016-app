510638a384b5f9f635abcdd9b00fc79238e11470
alphinanitta <alphinanitta@gmail.com>
2016-09-28 07:59:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,7 @@ function bu(){
   counter= counter + 1 ; 
   var span =  document.getElementById('span');
   span.innerHTML = counter.toString();
-  document.getElementById('span').innerHTML = counter.toString();
-  alert(counter);
+  
 }
 
 
