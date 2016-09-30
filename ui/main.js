a0a56a1c8dc0b94a6f3cc3d19bcaf9aab76969e7
alphinanitta <alphinanitta@gmail.com>
2016-09-30 20:46:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,21 @@
-
+var namein = document.getElementById('hi').value;
 function bu(){
   var request = new XMLHttpRequest();
   
   request.onreadystatechange   = function(){
     if(request.readyState === XMLHttpRequest.DONE){
         if(request.status===200){
-            var counter = request.responseText;
-            var span =  document.getElementById('span');
-            span.innerHTML = counter.toString();
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list='';
+            for (var i = 0 ;i <  names.length ; i++){
+                list += '<li>' + names[i] + '</li>';
+            }
+            (document.getElementById('one')).innerHTML = list ; 
         }
     }  
   };
-  request.open('GET','http://alphinanitta.imad.hasura-app.io/counter',true);
+  request.open('GET','http://alphinanitta.imad.hasura-app.io/submit-name'+ namein,true);
   request.send(null);
 }
 
